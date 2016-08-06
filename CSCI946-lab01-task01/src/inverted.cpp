d6413809f813e73eb66e56b392e4128ace72b9ba
Libaoxing <bl251@uowmail.edu.au>
2016-08-07 00:10:51 +1000

    20160806

--- CSCI946-lab01-task01/src/inverted.cpp
+++ CSCI946-lab01-task01/src/inverted.cpp
@@ -98,6 +98,7 @@ int main(int argc, char **argv) {
 			getline(inf, line);
 			// convert to lower case
 			transform(line.begin(), line.end(), line.begin(), ::tolower);
+			// bbbbb
 			// split word
 			size_t pos = line.find(" ");
 			while (pos != string::npos) {
