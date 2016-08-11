6a8aebeb1421794cbbd6bdee16f2ff6411d80b83
Libaoxing <bl251@uowmail.edu.au>
2016-08-11 20:19:33 +1000

    by Libaoxing

---
+++ CSCI946-lab02/src/searchv.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CSCI946-lab02.cpp
+// Author      : Baoxing Li
+// Version     :
+// Copyright   : bl251@uowmail.edu.au / sID: 5082497
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "lab02" << endl; // prints lab02
+	return 0;
+}
