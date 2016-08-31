41b5abf54f13ff4be35f99f80e128a06fcece66d
Libaoxing <bl251@uowmail.edu.au>
2016-09-01 00:53:12 +1000

    31/08/2016 By Li baoxing

---
+++ CSCI946-lab04-task1/src/CSCI946-lab04-task1.cpp
@@ -0,0 +1,137 @@
+//============================================================================
+// Name        : CSCI946-lab04-task1.cpp
+// Author      : Baoxing Li
+// Version     :
+// Copyright   : bl251@uowmail.edu.au / sID: 5083497
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include "Matrix.h"
+using namespace std;
+
+// a simple argument parser
+// can only parse -a xxx -b xxx -c xxx -d xxx
+class InputParser {
+private:
+	std::vector<string> tokens;
+public:
+	InputParser(int &argc, char **argv) {
+		for (int i = 1; i < argc; ++i)
+			this->tokens.push_back(string(argv[i]));
+	}
+
+	const string getCmdOption(const string &option) const {
+		vector<string>::const_iterator itr;
+		itr = find(this->tokens.begin(), this->tokens.end(), option);
+		if (itr != this->tokens.end() && ++itr != this->tokens.end()) {
+			return *itr;
+		}
+		return "";
+	}
+};
+
+template<class out_type, class in_value>
+out_type convert(const in_value & v) {
+	stringstream stream;
+	stream << v;
+	out_type result;
+	stream >> result;
+	return result;
+}
+
+int main(int argc, char ** argv) {
+	cout << endl << "              To calculate PageRank of a web graph" << endl; // prints Assignment
+	double teleportation_probability = 0;
+	string init_Matrix_File = "";
+	int Dimension = 7;
+	// xMatrix (1, Dimension);
+	Matrix<double> xMatrix(1, Dimension);
+	// pMatrix (Dimension, Dimension);
+	Matrix<double> pMatrix(Dimension, Dimension);
+	pMatrix(0, 2) = 1;
+	pMatrix(1, 1) = 1;
+	pMatrix(1, 2) = 1;
+	pMatrix(2, 0) = 1;
+	pMatrix(2, 2) = 1;
+	pMatrix(2, 3) = 1;
+	pMatrix(3, 3) = 1;
+	pMatrix(3, 4) = 1;
+	pMatrix(4, 6) = 1;
+	pMatrix(5, 5) = 1;
+	pMatrix(5, 6) = 1;
+	pMatrix(6, 3) = 1;
+	pMatrix(6, 4) = 1;
+	pMatrix(6, 6) = 1;
+	cout << endl << "   ----------------------- Step 0 aMatrix --------------------" << endl;
+	cout << pMatrix << endl;
+	double count[Dimension] = { 0 };
+	for (int r = 0; r < Dimension; ++r) {
+		for (int c = 0; c < Dimension; ++c) {
+			if (pMatrix(r, c) == 1)
+				count[r]++;
+		}
+		// dead ends
+		if (count[r] == 0)
+			count[r] = Dimension;
+	}
+	for (int r = 0; r < Dimension; ++r) {
+		for (int c = 0; c < Dimension; ++c) {
+			pMatrix(r, c) = pMatrix(r, c) / count[r];
+			//pMatrix(r, c) = ((double) ((int) ((pMatrix(r, c) + 0.005) * 100))) / 100;
+		}
+	}
+	cout << "   ----------------------- Step 1 aMatrix --------------------" << endl;
+	cout << pMatrix << endl;
+	InputParser argument(argc, argv);
+	try {
+		teleportation_probability = convert<double, string>(argument.getCmdOption("-t"));
+		init_Matrix_File = argument.getCmdOption("-v");
+		if (init_Matrix_File == "")
+			throw 500;
+	} catch (...) {
+		cout << "Invalid parameters." << endl;
+		cout << "(Usage) ./calcu_ pagerank.exe -t 0.07 -v init_vector.txt" << endl;
+		return 500;
+	}
+
+	cout << endl << "   ----------------------- Step 2 pMatrix --------------------" << endl;
+	// convert A Matrix to P Matrix
+	pMatrix = pMatrix * (1 - teleportation_probability) + (teleportation_probability * 1.0) / Dimension;
+	cout << pMatrix << endl;
+
+	fstream fp(init_Matrix_File.c_str(), ios::in);
+	if (fp.is_open()) {
+		// read initial vector x0 data from file
+		for (int c = 0; c < Dimension; ++c) {
+			fp >> xMatrix(0, c);
+			//xMatrix(0, c) = ((double) ((int) ((xMatrix(0, c) + 0.005) * 100))) / 100;
+			if (!fp.good())
+				break;
+		}
+		fp.close();
+	}
+	cout << endl << "   ---------------------- Step 3 xVextor ------------------------------" << endl;
+	cout << "   X000: " << xMatrix;
+	// get the steady states
+	Matrix<double> xMatrix0;
+	bool isOk = false;
+	int k = 0;
+	while (!isOk) {
+		xMatrix0 = xMatrix;
+		xMatrix = xMatrix * pMatrix;
+		k++;
+		cout << "   X" << setw(3) << setfill('0') << k << ": " << xMatrix;
+		if (xMatrix0 == xMatrix) //?? when is the xVector steady
+			break;
+	}
+
+	return 0;
+}
---
+++ CSCI946-lab04-task1/src/Matrix.h
@@ -0,0 +1,190 @@
+/*
+ * Matrix.h
+ *
+ *  Created on: 2016Äê8ÔÂ31ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <typeinfo>
+
+template<class T> class Matrix {
+	friend std::istream& operator>>(std::istream &in, Matrix<T> &obj) {
+		T item;
+		//release
+		if (obj.m_data != NULL) {
+			for (int i = 0; i < obj.getRows(); i++) {
+				delete[] obj.m_data[i];
+			}
+			delete[] obj.m_data;
+		}
+		//new
+		obj.m_data = new T *[obj.getRows()];
+		for (int i = 0; i < obj.getRows(); i++) {
+			obj.m_data[i] = new T[obj.getCols()];
+		}
+		//input
+		std::cout << "	Please input data for the ";
+		for (int r = 0; r < obj.getRows(); r++)
+			for (int c = 0; c < obj.getCols(); c++) {
+				in >> item;
+				obj(r, c) = item;
+			}
+		return in;
+	}
+
+	friend std::ostream& operator<<(std::ostream &out, const Matrix<T> &obj) {
+		out << std::fixed << std::setprecision(3) << std::setiosflags(std::ios::right) << std::setfill(' ');
+		for (int r = 0; r < obj.rows; r++) {
+			for (int c = 0; c < obj.cols; c++) {
+				out << std::setw(8);	//?? is it enough
+				out << obj(r, c) << " ";
+			}
+			out << std::endl;
+		}
+		return out;
+	}
+
+private:
+	T **m_data;
+	int rows;
+	int cols;
+public:
+	Matrix() {
+		rows = 0;
+		cols = 0;
+		m_data = NULL;
+	}
+
+	Matrix(int r, int c) {
+		rows = r;
+		cols = c;
+		// allocate memory for m_data[][]
+		if (r > 0 && c > 0) {
+			m_data = new T *[this->getRows()];
+			for (int i = 0; i < this->getRows(); i++) {
+				m_data[i] = new T[this->getCols()];
+			}
+
+			for (int r = 0; r < this->getRows(); r++) {
+				for (int c = 0; c < this->getCols(); c++) {
+					this->m_data[r][c] = 0;
+				}
+			}
+		} else {
+			m_data = NULL;
+		}
+	}
+
+	~Matrix() {
+		// deallocate memory
+		if (m_data != NULL) {
+			for (int i = 0; i < this->getRows(); i++) {
+				delete[] m_data[i];
+			}
+			delete[] m_data;
+		}
+	}
+
+	int getRows() const {
+		return this->rows;
+	}
+	int getCols() const {
+		return this->cols;
+	}
+	void setRows(int rows) {
+		this->rows = rows;
+	}
+	void setCols(int columns) {
+		this->cols = columns;
+	}
+
+	// get & set item(r,c)
+	T& operator()(const int r, const int c) {
+		return this->m_data[r][c];
+	}
+	const T& operator()(const int r, const int c) const {
+		return this->m_data[r][c];
+	}
+	// deep copy
+	Matrix<T>& operator=(const Matrix<T> &other) {
+		if (this == &other) {
+			return *this;
+		} else {
+			// release memory
+			for (int i = 0; i < this->getRows(); i++) {
+				delete[] this->m_data[i];
+			}
+			delete[] this->m_data;
+			// allocate memory
+			this->setRows(other.getRows());
+			this->setCols(other.getCols());
+			this->m_data = new T *[rows];
+			for (int i = 0; i < this->getRows(); i++) {
+				this->m_data[i] = new T[this->getCols()];
+			}
+			// copy data
+			for (int r = 0; r < this->getRows(); r++) {
+				for (int c = 0; c < this->getCols(); c++) {
+					this->m_data[r][c] = other.m_data[r][c];
+				}
+			}
+			return *this;
+		}
+	}
+	;
+
+	Matrix<T>& operator+(const double &param) {
+		for (int r = 0; r < this->getRows(); r++) {
+			for (int c = 0; c < this->getCols(); c++) {
+				this->m_data[r][c] = this->m_data[r][c] + param;
+			}
+		}
+		return *this;
+	}
+
+	Matrix<T>& operator*(const double &param) {
+		for (int r = 0; r < this->getRows(); r++) {
+			for (int c = 0; c < this->getCols(); c++) {
+				this->m_data[r][c] = this->m_data[r][c] * param;
+			}
+		}
+		return *this;
+	}
+
+	Matrix<T> operator*(const Matrix<T> &other) {
+		if (this->getCols() != other.getRows())
+			throw 2;
+		Matrix<T> result(this->getRows(), other.getCols());
+		for (int r = 0; r < this->getRows(); r++)
+			for (int c = 0; c < other.getCols(); c++) {
+				result.m_data[r][c] = 0;
+				for (int i = 0; i < this->getCols(); i++) {
+					result.m_data[r][c] = result.m_data[r][c] + this->m_data[r][i] * other.m_data[i][c];
+				}
+			}
+		return result;
+	}
+
+	bool operator==(const Matrix<T> &other) {
+		if (this->getRows() != other.getRows() || this->getCols() != other.getCols())
+			throw 2;
+		double a, b;
+		for (int r = 0; r < this->getRows(); ++r) {
+			for (int c = 0; c < this->getCols(); ++c) {
+				a = ((double) ((int) ((this->m_data[r][c] + 0.0005) * 1000))) / 1000;
+				b = ((double) ((int) ((other.m_data[r][c] + 0.0005) * 1000))) / 1000;
+				if (a != b)
+					return false;
+			}
+		}
+		return true;
+	}
+};
+
+#endif /* MATRIX_H_ */
